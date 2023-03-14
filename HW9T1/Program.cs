265dfbb1ff414ba7bbe90e0c0e0454683c8e56d8
Anna Nikolenko <nikolenko_anmai@mail.ru>
2023-03-14 18:57:25 +0300

    task1 - All Even Number Between m and n

---
+++ HW9T1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа 
+// в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+int InputUser(string message)
+{
+    System.Console.Write($"{message} = ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void AllEvenNumberBetween(int M, int N)
+{
+    if (M == N)
+        return;
+    Console.Write($"{N}");
+    if (M % 2 != 0) return;
+    AllEvenNumberBetween(M + 1, N - 1);
+    return;
+    AllEvenNumberBetween(M + 2, N - 1);
+}
+
+int numM = InputUser("Введите первое число: ");
+int numN = InputUser("Введите второе число: ");
+AllEvenNumberBetween(numM, numN);
+
