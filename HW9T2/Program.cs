0b1c346bdf4af450191e919759acde14fbc0cc43
Anna Nikolenko <nikolenko_anmai@mail.ru>
2023-03-14 18:58:26 +0300

    task 2 sum

---
+++ HW9T2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int InputUser(string message)
+{
+    System.Console.Write($"{message} = ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Sum(int M, int N)
+{
+    if (M==N) return M;
+    return (N + Sum(M, N - 1));
+}
+
+int numM = InputUser("Введите первое число: ");
+int numN = InputUser("Введите второе число: ");
+Console.WriteLine($"{Sum(numM, numN)}");
